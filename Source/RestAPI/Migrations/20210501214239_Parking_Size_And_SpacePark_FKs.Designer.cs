52aebb617eca362f7dcbff8bf4eddfd02ff3ce4e
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-01 23:50:36 +0200

    Added foreign key ids to parking

--- Source/RestAPI/Migrations/20210501214239_Parking_Size_And_SpacePark_FKs.Designer.cs
+++ Source/RestAPI/Migrations/20210501214239_Parking_Size_And_SpacePark_FKs.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestAPI.Data;
 
 namespace RestAPI.Migrations
 {
     [DbContext(typeof(SpaceParkDbContext))]
-    partial class SpaceParkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210501214239_Parking_Size_And_SpacePark_FKs")]
+    partial class Parking_Size_And_SpacePark_FKs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/RestAPI/Migrations/20210501214239_Parking_Size_And_SpacePark_FKs.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestAPI.Migrations
+{
+    public partial class Parking_Size_And_SpacePark_FKs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parkings_Sizes_SizeId",
+                table: "Parkings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parkings_SpaceParks_SpaceParkId",
+                table: "Parkings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpaceParkId",
+                table: "Parkings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SizeId",
+                table: "Parkings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parkings_Sizes_SizeId",
+                table: "Parkings",
+                column: "SizeId",
+                principalTable: "Sizes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parkings_SpaceParks_SpaceParkId",
+                table: "Parkings",
+                column: "SpaceParkId",
+                principalTable: "SpaceParks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parkings_Sizes_SizeId",
+                table: "Parkings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parkings_SpaceParks_SpaceParkId",
+                table: "Parkings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpaceParkId",
+                table: "Parkings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SizeId",
+                table: "Parkings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parkings_Sizes_SizeId",
+                table: "Parkings",
+                column: "SizeId",
+                principalTable: "Sizes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parkings_SpaceParks_SpaceParkId",
+                table: "Parkings",
+                column: "SpaceParkId",
+                principalTable: "SpaceParks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
