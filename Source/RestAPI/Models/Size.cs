04bedeaf973cb47ed561e5cd7d765d8d18bf387e
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-01 17:13:21 +0200

    Added Size Model

---
+++ Source/RestAPI/Models/Size.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models
+{
+    public class Size
+    {
+        public int Id { get; set; }
+        public ParkingSize Type { get; set; } 
+    }
+    public enum ParkingSize
+    {
+        Small = 1,
+        Medium = 2,
+        Large = 3,
+        VeryLarge = 4
+    }
+}
