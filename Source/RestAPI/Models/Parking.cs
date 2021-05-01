7e3e7929dc31b427537ea08245e987dd511172f9
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-01 17:12:59 +0200

    Added Parking Model

---
+++ Source/RestAPI/Models/Parking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models
+{
+    public class Parking
+    {
+        public int Id { get; set; }
+        public Size SizeId { get; set; }
+        public string CharacterName { get; set; }
+        public string SpaceshipName { get; set; }
+        public DateTime Arrival { get; set; }
+    }
+
+}
