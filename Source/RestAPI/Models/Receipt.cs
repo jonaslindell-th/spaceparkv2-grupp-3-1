9d746ad8c18ddfb6f1daa1e9c12d8e52dd19a56f
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-01 17:13:13 +0200

    Added Receipt Model

---
+++ Source/RestAPI/Models/Receipt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models
+{
+    public class Receipt
+    {
+        public int ID { get; set; }
+        public Size SizeId { get; set; }
+        public string Name { get; set; }
+        public string StarshipName { get; set; }
+        public DateTime Arrival { get; set; }
+        public DateTime Departure { get; set; }
+        public double TotalAmount { get; set; }
+    }
+}
