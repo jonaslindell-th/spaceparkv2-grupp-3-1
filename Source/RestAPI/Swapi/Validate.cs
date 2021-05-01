abe198c5615c5db8700b604e6e8189015acce864
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-01 14:51:01 +0200

    Added Validate.Person method

---
+++ Source/RestAPI/Swapi/Validate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace RestAPI.Swapi
+{
+    public class Validate
+    {
+        public static async Task<bool> Person(string input)
+        {
+            var client = new RestClient("https://swapi.dev/api/");
+            var request = new RestRequest($"people/?search={input}", DataFormat.Json); // Use the swAPI search function with the user input string.
+            var peopleResponse = await client.GetAsync<PeopleResponse>(request); // Add the results from the search to the PeopleResponse List
+            var person = peopleResponse.Results.Find(p => p.Name.ToLower() == input.ToLower());
+            return person != null;
+        }
+    }
+}
