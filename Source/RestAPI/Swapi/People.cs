47f86cd01f2f6618f5cdc512fecf54340bdfb3bd
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-01 14:50:35 +0200

    Added swapi models and restsharp nugetpackage

---
+++ Source/RestAPI/Swapi/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Swapi
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public List<string> StarShips { get; set; }
+    }
+}
---
+++ Source/RestAPI/Swapi/PeopleResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Swapi
+{
+    public class PeopleResponse
+    {
+        public List<People> Results { get; set; }
+    }
+}
