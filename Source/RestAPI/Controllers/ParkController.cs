9f014f9804af77bfcb96916386273aab5098ec85
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-02 13:43:34 +0200

    Added Park endpoint and parkrequest class

--- Source/RestAPI/Controllers/ParkController.cs
+++ Source/RestAPI/Controllers/ParkController.cs
@@ -3,15 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using RestAPI.Data;
+using RestAPI.Models;
+using RestAPI.Requests;
+using RestAPI.Swapi;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace RestAPI.Controllers
 {
-    [Route("park/[controller]")]
+    [Route("/api/[controller]")]
     [ApiController]
     public class ParkController : ControllerBase
     {
+        private SpaceParkDbContext _dbContext;
+
+        public ParkController(SpaceParkDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         // GET: api/<ParkController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -34,8 +46,29 @@ namespace RestAPI.Controllers
 
         // PUT api/<ParkController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] ParkRequest request)
         {
+            var isValid = Validate.Person(request.PersonName);
+            if (isValid.Result)
+            {
+                var foundParking = _dbContext.Parkings.FirstOrDefault(p => p.Id == id);
+                if (foundParking != null)
+                {
+                    foundParking.Arrival = DateTime.Now;
+                    foundParking.CharacterName = request.PersonName;
+                    foundParking.SpaceshipName = request.ShipName;
+                    _dbContext.SaveChanges();
+                    return StatusCode(StatusCodes.Status200OK, "Vehicle parked.");
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, "Parking was not found.");
+                }
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "Not a valid character or ship");
+            }
         }
 
         // DELETE api/<ParkController>/5
---
+++ Source/RestAPI/Requests/ParkRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Requests
+{
+    public class ParkRequest
+    {
+        public string PersonName { get; set; }
+        public string ShipName { get; set; }
+    }
+}
