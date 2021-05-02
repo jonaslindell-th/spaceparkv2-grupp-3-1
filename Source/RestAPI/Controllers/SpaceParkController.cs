1fc663d75a90771745f947c407c0c99b7ff6c46b
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-02 14:09:14 +0200

    Park url changes

--- Source/RestAPI/Controllers/SpaceParkController.cs
+++ Source/RestAPI/Controllers/SpaceParkController.cs
@@ -4,32 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using RestAPI.Data;
+using RestAPI.Models;
+using RestAPI.Requests;
 using RestAPI.Swapi;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace RestAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("/api/[controller]")]
     [ApiController]
     public class SpaceParkController : ControllerBase
     {
-        // GET: api/<SpaceParkController>
-        [HttpGet]
-        public IActionResult Get([FromBody] string name)
+        private SpaceParkDbContext _dbContext;
+
+        public SpaceParkController(SpaceParkDbContext dbContext)
         {
-            var isValid = Validate.Person(name);
-            if (isValid.Result)
-            {
-             return StatusCode(StatusCodes.Status200OK, "Person Validated");
-            }
-            else
-            {
-                return StatusCode(StatusCodes.Status401Unauthorized, "Not a authorized person");
-            }
+            _dbContext = dbContext;
         }
 
-        //GET api/<SpaceParkController>/5
+        // GET: api/<SpaceParkController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SpaceParkController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
@@ -43,9 +45,31 @@ namespace RestAPI.Controllers
         }
 
         // PUT api/<SpaceParkController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("park/{id}")]
+        public IActionResult Park(int id, [FromBody] ParkRequest request)
         {
+            var validPerson = Validate.Person(request.PersonName);
+            var validShip = Validate.Starship(request.ShipName);
+            if (validPerson.Result && validShip.Result)
+            {
+                var foundParking = _dbContext.Parkings.FirstOrDefault(p => p.Id == id);
+                if (foundParking != null)
+                {
+                    foundParking.Arrival = DateTime.Now;
+                    foundParking.CharacterName = request.PersonName;
+                    foundParking.SpaceshipName = request.ShipName;
+                    _dbContext.SaveChanges();
+                    return StatusCode(StatusCodes.Status200OK, "Vehicle parked.");
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, "Parking was not found.");
+                }
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "Not a valid character or ship");
+            }
         }
 
         // DELETE api/<SpaceParkController>/5
