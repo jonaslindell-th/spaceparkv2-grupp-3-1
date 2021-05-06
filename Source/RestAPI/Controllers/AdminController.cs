375f65a1b3512afc5a07b0c2682c8835f32bdedd
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-05-06 13:22:04 +0200

    Moved managespaceport endpoints to admincontroller

--- Source/RestAPI/Controllers/AdminController.cs
+++ Source/RestAPI/Controllers/AdminController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using RestAPI.Data;
 using RestAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,10 +18,14 @@ namespace RestAPI.Controllers
     public class AdminController : ControllerBase
     {
         private SpaceParkDbContext _dbContext;
+        private ISpacePort _spacePort { get; set; }
+        private IParking _parking { get; set; }
 
-        public AdminController(SpaceParkDbContext dbContext)
+        public AdminController(SpaceParkDbContext dbContext, ISpacePort spacePort, IParking parking)
         {
             _dbContext = dbContext;
+            _spacePort = spacePort;
+            _parking = parking;
         }
 
         // POST api/Admin/AddParking
@@ -56,6 +61,73 @@ namespace RestAPI.Controllers
             {
                 return BadRequest($"Parking with id:{id} was not found.");
             }
+        }
+
+        // GET: api/ManageSpacePorts/GetAllSpacePorts
+        [HttpGet("[action]")]
+        public IActionResult GetAllSpacePorts()
+        {
+            var spacePorts = _dbContext.SpacePorts.Include("Parkings").ToList();
+
+            return Ok(spacePorts);
+        }
+
+        // POST api/ManageSpacePorts/AddSpacePort
+        [HttpPost("[action]")]
+        public IActionResult AddSpacePort([FromBody] string name)
+        {
+            try
+            {
+                _spacePort.Name = name;
+                _spacePort.Parkings = new List<Parking>();
+                for (int i = 0; i < 5; i++)
+                {
+                    _parking = new Parking();
+                    _parking.SizeId = _dbContext.Sizes.Where(s => s.Type == ParkingSize.Small).Select(s => s.Id).FirstOrDefault();
+                    _spacePort.Parkings.Add((Parking)_parking);
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    _parking = new Parking();
+                    _parking.SizeId = _dbContext.Sizes.Where(s => s.Type == ParkingSize.Medium).Select(s => s.Id).FirstOrDefault();
+                    _spacePort.Parkings.Add((Parking)_parking);
+                }
+                for (int i = 0; i < 2; i++)
+                {
+                    _parking = new Parking();
+                    _parking.SizeId = _dbContext.Sizes.Where(s => s.Type == ParkingSize.Large).Select(s => s.Id).FirstOrDefault();
+                    _spacePort.Parkings.Add((Parking)_parking);
+                }
+
+                _parking = new Parking();
+                _parking.SizeId = _dbContext.Sizes.Where(s => s.Type == ParkingSize.VeryLarge).Select(s => s.Id).FirstOrDefault();
+                _spacePort.Parkings.Add((Parking)_parking);
+                _dbContext.SpacePorts.Add((SpacePort)_spacePort);
+
+                _dbContext.SaveChanges();
+
+                return StatusCode(StatusCodes.Status201Created, $"SpacePort added id: {_spacePort.Id} name: {_spacePort.Name}");
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "An error has occurred while adding a new spaceport.");
+            }
+        }
+
+        // DELETE api/ManageSpacePorts/DeleteSpacePort/5
+        [HttpDelete("[action]/{id}")]
+        public IActionResult DeleteSpacePort(int id)
+        {
+            //TODO: Interface för spaceport
+            SpacePort spacePort = _dbContext.SpacePorts.FirstOrDefault(s => s.Id == id);
+            if (spacePort != null)
+            {
+                _dbContext.SpacePorts.Remove(spacePort);
+                _dbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status202Accepted, $"Space port deleted.");
+            }
+
+            return BadRequest("Space port was not found.");
         }
     }
 }
